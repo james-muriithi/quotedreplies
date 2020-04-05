a20b1a0d8a128b1592c8ba56902b01e323b7b5ae
james-muriithi <muriithijames556@gmail.com>
2020-04-05 23:57:52 +0300

    updated index page

--- js/main.js
+++ js/main.js
@@ -1,38 +1,42 @@
-//Global variable for starting page
-var currentPageId = "page-home";
-var currentSelectorId = "home";
+//Global letiable for starting page
+let currentPageId = "page-home";
+let currentSelectorId = "home";
 
 //Function for getting the button ids
-function getButtons() {
+const getButtons = ()=> {
     //List of button ids
-    var list = ["home", "feed", "create", "account"];
-    return list;
+    return ["home", "popular", "about", "account"];
+     
 }
 // service worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
+    window.addEventListener('load', () => {
         navigator.serviceWorker.register('service-worker.js')
-            .then(function() {
+            .then(()=> {
                 console.log("Service Worker Registered,");
             });
     });
 }
 
 //Make sure the window is loaded before we add listeners
-window.onload = function() {
-    var pageIdList = getButtons();
+window.onload = ()=> {
+    let pageIdList = getButtons();
     //Add an event listener to each button
-    pageIdList.forEach(function(page) {
+    pageIdList.forEach(page => {
         document.getElementById(page).addEventListener("click", changePage, false);
+    });
+    $('.navbar-nav').children().each(function(index, el) {
+        $(el).on('click', changePage);
     });
 }
 
-function changePage() {
-    var currentSelector = document.getElementById(currentSelectorId);
-    var currentPage = document.getElementById(currentPageId);
-    var pageId = "page-" + this.id;
-    var page = document.getElementById(pageId);
-    var pageSelector = document.getElementById(this.id);
+const changePage = function() {
+    id = this.id ? this.id : this.dataset.trigger
+    let currentSelector = document.getElementById(currentSelectorId);
+    let currentPage = document.getElementById(currentPageId);
+    let pageId = "page-" + id;
+    let page = document.getElementById(pageId);
+    let pageSelector = document.getElementById(id);
 
     if (page.classList.contains("active")) {
         return;
@@ -52,6 +56,21 @@ function changePage() {
     //Need to reset the scroll
     window.scrollTo(0, 0);
 
-    currentSelectorId = this.id;
+    currentSelectorId = id;
     currentPageId = pageId;
+
+    $('.navbar-nav').children().removeClass('active')
+    $('.navbar-nav').find(`[data-trigger='${id}']`).addClass('active');
 }
+
+function generateLink (url) {
+            let parts = url.match(/(^|[^'"])(https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+))/);
+            try {
+                let tweetId = parts[4]
+                let link = `https://twitter.com/search?q=-from:quoted_replies%20url:${tweetId}`
+                return [link, tweetId];
+
+            } catch (error) {
+                return[null, null];
+            }
+        }
